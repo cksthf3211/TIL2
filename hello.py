fe7fb30be70cd5078f9213be0ca4fff99fb398d7
cksthf3211 <cksthf321@gmail.com>
2023-02-09 11:54:01 +0900

    백준 1436

--- hello.py
+++ hello.py
@@ -2,22 +2,42 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n, k = map(int, input().split()) # n: 수빈 k: 동생
-graph = [0] * 100001
+n, m = map(int, input().split())
+string = [list(map(str, input().strip())) for _ in range(n)]
+paint = []
 
-def bfs():
-    queue = deque([n])
+# print(string)
+# [['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+#  ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+#  ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+#  ['B', 'W', 'B', 'B', 'B', 'W', 'B', 'W'], 
+#  ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+#  ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W'],
+#  ['W', 'B', 'W', 'B', 'W', 'B', 'W', 'B'],
+#  ['B', 'W', 'B', 'W', 'B', 'W', 'B', 'W']]
 
-    while queue:
-        x = queue.popleft()
+# 반복문을 통해 체스판을 확인(8*8 범위로 자름)
+for i in range(n-7):
+    for j in range(m-7):
+        w = 0
+        b = 0
 
-        if x == k: # 위치가 같다면
-            print(graph[x]) # 그 위치를 출력
-            break
-        
-        for i in (x - 1, x + 1, x * 2):
+        # 8*8 체스판 확인
+        for k in range(i, i+8):
+            for l in range(j, j+8):
+                if (l + k) % 2 == 0: # 짝수
+                    if string[k][l] != 'W':
+                        w += 1
+                    else:
+                        b += 1
+                else:
+                    if string[k][l] != 'B':
+                        w += 1
+                    else:
+                        b += 1
 
-            if 0 <= i <= 100000 and graph[i] == 0:
-                graph[i] = graph[x] + 1
-                queue.append(i)
-bfs()
+        paint.append(w)
+        paint.append(b)
+
+# 칠한 색 중에 최솟값
+print(min(paint))
