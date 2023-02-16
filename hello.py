f857a5c218f9dc93cddebe0f471094bf51eb7299
cksthf3211 <cksthf321@gmail.com>
2023-02-16 10:22:05 +0900

    백준 15650 백트래킹

--- hello.py
+++ hello.py
@@ -6,15 +6,15 @@ n, m= map(int, input().split())
 n_list = []
 
 # 재귀함수 이용
-def dfs():
+def dfs(start):
     if len(n_list) == m:
         print(' '.join(map(str, n_list)))
         return
     
-    for i in range(1, n+1):
+    for i in range(start, n+1):
         if i not in n_list:
             n_list.append(i)
-            dfs()
+            dfs(i + 1)
             n_list.pop()
 
-dfs()
+dfs(1)
