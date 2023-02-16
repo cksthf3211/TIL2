9e50f11bc3e9b3e6417940a310fb312b19e9c6e5
cksthf3211 <cksthf321@gmail.com>
2023-02-16 09:41:45 +0900

    백준 15649 백트래킹

--- hello.py
+++ hello.py
@@ -2,31 +2,19 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n = int(input())
-n_list = sorted(list(map(int, input().split())))
-x = int(input())
+n, m= map(int, input().split())
+n_list = []
 
-left, right = 0, n-1
-cnt = 0
+# 재귀함수 이용
+def dfs():
+    if len(n_list) == m:
+        print(' '.join(map(str, n_list)))
+        return
+    
+    for i in range(1, n+1):
+        if i not in n_list:
+            n_list.append(i)
+            dfs()
+            n_list.pop()
 
-# print(n)
-# print(n_list)
-# print(x)
-# 9
-# [1, 2, 3, 5, 7, 9, 10, 11, 12]
-# 13
-
-while left < right: # 왼쪽, 오른쪽 방향의 인덱스가 서로 만날 때(교차할 때)까지 진행
-    # 두 수의 합이 x보다 큰 경우 - 더 큰 값을 더해야하므로 left+1
-    # 두 수의 합이 x보다 작은 경우 - 더 작은 값을 더해야하므로 right-1
-    # 두 수의 합이 x인 경우 - answer+1 & left+1
-    temp = n_list[left] + n_list[right]
-    if temp == x:
-        cnt += 1
-        left += 1
-    elif temp < x:
-        left += 1
-    else:
-        right -= 1
-
-print(cnt)
+dfs()
