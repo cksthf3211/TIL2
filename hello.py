0bed1bec7c685b602e3da51e7ab35c20c7d53a8d
cksthf3211 <cksthf321@gmail.com>
2023-02-20 16:04:21 +0900

    백준 11478

--- hello.py
+++ hello.py
@@ -2,19 +2,22 @@ import sys
 input = sys.stdin.readline
 from collections import deque
 
-n, m= map(int, input().split())
-n_list = []
+# n, m= map(int, input().split())
+# s = input()
 
-# 재귀함수 이용
-def dfs(start):
-    if len(n_list) == m:
-        print(' '.join(map(str, n_list)))
-        return
-    
-    for i in range(start, n+1):
-        if i not in n_list:
-            n_list.append(i)
-            dfs(i + 1)
-            n_list.pop()
+# ans = []
 
-dfs(1)
+# for i in range(len(s)):
+#     for j in range(i, len(s)):
+#         ans.append(set(s[i:j + 1]))
+
+# print(len(ans))
+
+s = input()
+ans = set()
+
+for i in range(len(s)):
+    for j in range(i, len(s)):
+        ans.add(s[i:j + 1])
+
+print(len(ans))
