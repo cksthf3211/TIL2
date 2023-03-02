82611d2b27a6849cb833e397da6450bfc99754c3
cksthf3211 <cksthf321@gmail.com>
2023-03-02 10:43:32 +0900

    수정;

--- hello.py
+++ hello.py
@@ -4,6 +4,15 @@ input = sys.stdin.readline
 from collections import deque
 import pprint
 
-# n, m= map(int, input().split())
-n = list(map(int, input().split()))
-print(sum(n)) 
+# n, m = map(int, input().split())
+# n = list(map(int, input().split()))
+
+# 산업현장 안전관리 점검 인공진능(AI)기술을 이용한 과제를 수행하기 위해
+# 스마트안전고리와 스마트밴드를 사용하고싶습니다.
+# 연결방식이 어떻게 되는지 궁금하고
+# 앱을 따로 개발해서 기기와 연결해서 사용할 수 있는지 궁금합니다.
+# API를 지원하나요?
+# 오프라인에서도 사용 할 수 있나요?
+# 견적서를 보내주십시오.
+# 스마트안전고리 9개
+# 스마트밴드 2개
