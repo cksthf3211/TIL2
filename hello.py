e0cce43835136212d40c2d4ea332bc710b9a8429
cksthf3211 <cksthf321@gmail.com>
2023-03-09 10:27:44 +0900

    알람 마지막 수정

--- hello.py
+++ hello.py
@@ -4,59 +4,56 @@ input = sys.stdin.readline
 from collections import deque
 from pprint import pprint
 
-import calendar
-today = calendar.datetime.date.today()
-print(calendar.month(today.year, today.month))
+# import calendar
+# today = calendar.datetime.date.today()
+# print(calendar.month(today.year, today.month))
 
-import datetime
-now = datetime.datetime.now()
-print("현재 시간은 ", now.strftime('%Y-%m-%d %H:%M'))
+# import datetime
+# now = datetime.datetime.now()
+# print("현재 시간은 ", now.strftime('%Y-%m-%d %H:%M'))
 
 
 # n, m = map(int, input().split())
 # n = list(map(int, input().split()))
 
-# import time
-# import winsound
 
-# # 17시59분이 되면 알람 울리기
-# while True:
-#     now = time.localtime()
-#     if now.tm_hour == 11 and now.tm_min == 19:
-#         print("알람 울립니다!")
-#         winsound.Beep(440, 1000) # 440 Hz 주파수로 1초 동안 소리 울리기
-#         break
-#     else:
-#         time.sleep(60) # 1분 대기 후 다시 검사 ctrl + c로 종료
-
+import calendar
+import datetime
 import time
 import webbrowser
 import os
 
-# # 17시59분이 되면 알람 울리기
-# while True:
-#     now = time.localtime()
-#     if now.tm_hour == 11 and now.tm_min == 37:
-#         print("알람 울립니다!")
-#         url = "https://www.youtube.com/watch?v=lqFby1rSGyI"
-#         webbrowser.open(url)
-#         break
-#     else:
-#         time.sleep(100)
-#         print("알람 다시 울립니다!")
+today = calendar.datetime.date.today()
+print(calendar.month(today.year, today.month))
 
-# 17시59분이 되면 알람 울리기
+now = datetime.datetime.now()
+print("현재 시간은 ", now.strftime('%Y-%m-%d %H:%M'))
+
 while True:
     now = time.localtime()
-    if now.tm_hour == 11 and now.tm_min == 53:
+    if now.tm_hour == 17 and now.tm_min == 59:
         print("알람 울립니다!")
-        url = "https://www.youtube.com/watch?v=lqFby1rSGyI"
+        url = "https://youtu.be/xbO1CY1PXkE"
         webbrowser.open(url)
         time.sleep(30)
         os.system("taskkill /im chrome.exe /f")
-        time.sleep(60)     # 1분 대기
-        url = "https://www.youtube.com/watch?v=lqFby1rSGyI"
+        time.sleep(60)                                 # 1분 대기
+        print("마지막 알람입니다!")
+        url = "https://youtu.be/xbO1CY1PXkE"
         webbrowser.open(url)
+        now = datetime.datetime.now()
+        print("현재 시간은 ", now.strftime('%Y-%m-%d %H:%M'))
         time.sleep(30)
-        break
-        # os.system("shutdown /s /t 1")
+        print("시스템 종료합니다!")
+        os.system('taskkill /f /im explorer.exe')      # 시스템 종료
+        os.system('taskkill /f /im MicrosoftEdge.exe') # 시스템 종료
+        os.system('taskkill /f /im chrome.exe')        # 시스템 종료
+        print("30초 후 컴퓨터 종료합니다!")
+        print("현재 시간은 ", now.strftime('%Y-%m-%d %H:%M'), "오늘도 고생하셨습니다.")
+        cnt = 0
+        while cnt < 60:
+            cnt += 1
+            time.sleep(1)
+            print(cnt)
+
+            os.system('shutdown /s /t 0')              # 컴퓨터 종료
