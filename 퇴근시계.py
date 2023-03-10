210b6a99f4fa1abd976d64a6d158fbdb3bddc03c
cksthf3211 <cksthf321@gmail.com>
2023-03-10 10:44:15 +0900

    수정

--- 퇴근시계.py
+++ 퇴근시계.py
@@ -16,15 +16,15 @@ while True:
         print("알람 울립니다!")
         url = "https://youtu.be/xbO1CY1PXkE"
         webbrowser.open(url)
-        time.sleep(30)
+        time.sleep(30)                                 # 30초 뒤에 알람 꺼짐(chrome 다 꺼짐)
         os.system("taskkill /im chrome.exe /f")
-        time.sleep(60)                                 # 1분 대기
+        time.sleep(60)                                 # 1분 대기 후 알람
         print("마지막 알람입니다!")
         url = "https://youtu.be/xbO1CY1PXkE"
         webbrowser.open(url)
         now = datetime.datetime.now()
         print("현재 시간은", now.strftime('%Y-%m-%d %H:%M'), '입니다.')
-        time.sleep(30)
+        time.sleep(30)                                 # 30초 후에 시스템 및 컴퓨터 종료
         print("시스템 종료합니다!")
         os.system('taskkill /f /im explorer.exe')      # 시스템 종료
         os.system('taskkill /f /im MicrosoftEdge.exe') # 시스템 종료
