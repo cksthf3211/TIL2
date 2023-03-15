fddb0d9b62aef04efcad051ff0131c8f10b37c9c
cksthf3211 <cksthf321@gmail.com>
2023-03-15 09:07:43 +0900

    백준

--- hello.py
+++ hello.py
@@ -7,17 +7,6 @@ from pprint import pprint
 
 # n = list(map(int, input().split()))
 
-n, m = map(int, input().split())  # 공의 개수, 바구니의 수
-basket = [0 for i in range(n)]    # 바구니 초기화
-# print(basket) [0, 0, 0, 0, 0]
-
-
-for i in range(m):
-    a, b, c = map(int, input().split())  # a부터 b범위까지 c 번호만 넣음
-
-    for j in range(a, b+1):       # a부터 b범위
-        basket[j-1] = c           # 첫번째 바구니의 번호가 1번이고 배열의 첫번째 인덱스는 0이므로 i번째 배열의 값을 대입해주기위해  basket[j-1] = c 로 표현
-
-# for k in basket:
-#     print(k, end = ' ')
-print(*basket)
+for _ in range(int(input())):
+    A, B = map(int, input().split())
+    print("yes")
