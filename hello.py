566fa8066e566ff88d617ec22bf1477c4d056404
cksthf3211 <cksthf321@gmail.com>
2023-04-04 15:54:07 +0900

    백준

--- hello.py
+++ hello.py
@@ -1,5 +1,5 @@
 import sys
-# sys.stdin = open('hello.txt')
+sys.stdin = open('hello.txt')
 input = sys.stdin.readline
 from collections import deque
 from pprint import pprint
@@ -7,9 +7,11 @@ from pprint import pprint
 
 # n = list(map(int, input().split()))
 
+n = int(input())
+for i in range(n):
+    num, s = input().split()
+    text = ''
+    for j in s:
+        text += int(num) * j
 
-while True:
-    try:
-        print(input())
-    except EOFError:
-        break
+    print(text)
