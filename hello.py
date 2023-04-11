1b10826abaff7bb81eb8b90e677f646e6cdce732
cksthf3211 <cksthf321@gmail.com>
2023-04-11 09:48:25 +0900

    수정

--- hello.py
+++ hello.py
@@ -8,14 +8,13 @@ from pprint import pprint
 n, m = map(int, input().split())
 
 box = [i for i in range(1, n+1)]
-# print(box) [1, 2, 3, 4]
+# print(box) [1, 2, 3, 4, 5] 공을 담을 박스 만듦
 
 for i in range(m):
     i, j = map(int, input().split())
     temp = box[i-1]
-    # print(temp) 1 3 1 2
+    # print(temp) 1 3 2 1 박스에 공을 넣음음
     box[i-1] = box[j - 1]
     box[j - 1] = temp
-
-for j in box:
+for j in box: # [3, 1, 4, 2, 5]
     print(j, end=" ")
