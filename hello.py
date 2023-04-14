e94b06f498545b526d408ab8761d22fc806d652c
cksthf3211 <cksthf321@gmail.com>
2023-04-14 09:17:31 +0900

    백준 5073

--- hello.py
+++ hello.py
@@ -5,6 +5,17 @@ from collections import deque
 from pprint import pprint
 
 
-a = int(input())
-for i in range(1, a+1):
-    print(i)
+while True:
+    a, b, c = map(int, input().split())
+
+    if a == b == c == 0:
+        break
+
+    if a + b <= c or a + c <= b or b + c <= a:
+        print("Invalid")
+    elif a == b == c:
+        print("Equilateral")
+    elif a == b or b == c or c == a:
+        print("Isosceles")
+    else:
+        print("Scalene")
