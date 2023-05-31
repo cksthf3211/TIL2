9de1770d7f43173dd741379fbff4a5a933a68560
cksthf3211 <cksthf321@gmail.com>
2023-05-31 09:27:45 +0900

    백준 24: 시간계산

--- hello.py
+++ hello.py
@@ -5,11 +5,16 @@ from collections import deque
 from pprint import pprint
 
 
-color = ['black', 'brown', 'red', 'orange', 'yellow', 'green', 'blue', 'violet', 'grey', 'white']
+h1, m1, s1 = map(int, input().split(':'))
+h2, m2, s2 = map(int, input().split(':'))
 
-f = color.index(input().strip())
-s = color.index(input().strip())
-t = color.index(input().strip())
+t = h2 * 3600 + m2 * 60 + s2 - ( h1 * 3600 + m1 * 60 + s1 )
 
-print(int(str(f) + str(s)) * (10 ** t))
-print("Project")
+if t < 0:
+    t += 60*60*24
+
+h = t // 3600 
+m = ( t % 3600 ) // 60 
+s = t % 60
+
+print( "%02d:%02d:%02d" % (h,m,s) )
